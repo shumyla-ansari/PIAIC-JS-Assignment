a7c4f8cea4295024d4d5494c741035aa8cb8832d
Shumyla <shumyla.fahd@hotmail.com>
2021-01-09 16:30:57 +0000

    chp_1_2_3 complete

---
+++ chp_123/index.js
@@ -0,0 +1,9 @@
+//declare a variable for String 'firstName' 
+
+var firstName = 'Shumyla';
+
+var lastName = 'Ansari';
+
+var age = 30;
+alert(`Hello ${firstName} ${lastName}, you are ${age} years old`);
+
