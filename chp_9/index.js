fde2a536fb60280db156e3cb5c4b0da0d3d8e317
Shumyla Ansari <shumyla@Shumylas-MacBook-Pro.local>
2021-02-28 22:52:22 +0000

    chap 9

---
+++ chp_9/index.js
@@ -0,0 +1,4 @@
+var firstName = prompt("Please enter first name");
+var secondName = prompt("Please enter last name");
+var age = prompt("Please enter age");
+alert("Hello " + firstName + " " + secondName +". You are " + age + " years old." )
