fd2d12856d75f4ccf8b6957457affae8ac1255a8
Shumyla Ansari <shumyla.fahd@hotmail.com>
2021-03-05 20:54:02 +0000

    chap_10_14

--- chp_10-14/index.js
+++ chp_10-14/index.js
@@ -19,3 +19,19 @@ if (operator === "+") {
 alert(
   "Your result is :" + firstNumber + operator + secondNumber + "=" + result
 );
+
+//-------------assignment 2 chp 10-14 -----------------//
+
+var course1 = parseInt(prompt("Please enter course 1 marks"));
+var course2 = parseInt(prompt("Please enter course 2 marks"));
+var course3 = parseInt(prompt("Please enter course 3 marks"));
+var course4 = parseInt(prompt("Please enter course 4 marks"));
+var course5 = parseInt(prompt("Please enter course 5 marks"));
+var obtainedMarks = course1+ course2+ course3+ course4+ course5;
+var totalMarks = 500;
+var total = obtainedMarks * 100/ totalMarks
+
+
+alert(
+  "Your result is : " + total + "%"
+);
