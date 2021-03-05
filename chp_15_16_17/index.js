dec0d23faf9529ff3916aeaee9c926bb753dcd0a
Shumyla Ansari <shumyla.fahd@hotmail.com>
2021-03-05 23:07:32 +0000

    chap_15_16_17

---
+++ chp_15_16_17/index.js
@@ -0,0 +1,37 @@
+var totalCourses = [];
+
+var course1 = prompt("course1, ");
+var course2 = prompt("course2, ");
+var course3 = prompt("course3, ");
+var course4 = prompt("course4, ");
+var course5 = prompt("course5, ");
+
+totalCourses.push(course1, course2, course3, course4, course5)
+alert(totalCourses);
+
+let totalNewCourses = [];
+var course1 = prompt("course1", course1);
+var course2 = prompt("course2", course2);
+var course3 = prompt("course3", course3);
+var course4 = prompt("course4", course4);
+var course5 = prompt("course5", course5);
+totalNewCourses.push(course1, course2, course3, course4, course5)
+if (totalCourses[0] !== totalCourses[0]){
+  totalCourses.splice(courses[0], 1, course1)
+} else if 
+(totalCourses[1] !== totalCourses[1]){
+  totalCourses.splice(courses[1], 1, course2)
+} else if 
+(totalCourses[2] !== totalCourses[2]){
+  totalCourses.splice(courses[2], 1, course1)
+} else if 
+ (totalCourses[3] !== totalCourses[3]){
+  totalCourses.splice(courses[3], 1, course1)
+} else if 
+(totalCourses[4] !== totalCourses[4]){
+  totalCourses.splice(courses[4], 1, course1)
+} 
+
+
+alert(totalNewCourses);
+
