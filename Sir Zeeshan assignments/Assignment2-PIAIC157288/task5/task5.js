34d21f98da4e5ceba23de5e9f3ddbf24ccfdb341
Shumyla Ansari <shumyla.fahd@hotmail.com>
2021-03-10 21:07:27 +0000

    task 5, 6 added

--- Sir Zeeshan assignments/Assignment2-PIAIC157288/task5/task5.js
+++ Sir Zeeshan assignments/Assignment2-PIAIC157288/task5/task5.js
@@ -1,4 +1,9 @@
-var age;
-age = 10;
+var secretNum = 3;
+var inputNum = parseInt(prompt("Please enter number between 1 to 10"));
 
-alert("i am " + age + " years old!")
+
+if (inputNum === secretNum ) {
+    document.write("Bingo..!! You won!!!")
+}  else if (++inputNum === secretNum ) {
+    document.write(" close enough!")
+}  
--- Sir Zeeshan assignments/Assignment2-PIAIC157288/task6/task6.js
+++ Sir Zeeshan assignments/Assignment2-PIAIC157288/task6/task6.js
@@ -1,4 +1,9 @@
-var birthYear;
-birthYear = 10;
 
-document.write("My birthYear is  " + birthYear + " \n data type of my declared variable is " + typeof(birthYear) );
+var inputNum = parseInt(prompt("Please enter number between 1 to 10"));
+var remainder = inputNum % 2;
+
+if (remainder === 0 ) {
+    document.write("Even")
+}  else  {
+    document.write(" Odd!")
+}
