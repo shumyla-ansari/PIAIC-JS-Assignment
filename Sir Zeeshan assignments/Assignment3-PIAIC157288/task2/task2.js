b11ee0f215c399b137af7f33f7fe07fc65ac2142
Shumyla Ansari <shumyla.fahd@hotmail.com>
2021-03-12 10:20:00 +0000

    add task 2- assignment 3

--- Sir Zeeshan assignments/Assignment3-PIAIC157288/task2/task2.js
+++ Sir Zeeshan assignments/Assignment3-PIAIC157288/task2/task2.js
@@ -1,9 +1,16 @@
-var color = prompt("Please color");
- 
-if (color === "Red") {
-   alert( "Must Stop")
-} else if (color === "Yellow") {
-    alert("Ready to move")
-} else if (color === "Green") {
-    alert("Move Now")
+var students = ["Michael", "John", "Tony"];
+var scores = [320, 230, 480];
+
+var total = 500;
+
+var percentage = [];
+
+for (var i = 0; i < scores.length; i++) {
+    percentage[i] = scores[i] * 100 / total
 };
+
+console.log(percentage);
+
+for (var i=0; i < students.length; i++){
+    document.write("Score of " + students[i] + " is " + scores[i] + ". Percentage is " + percentage[i]+ "%<br>")
+}
